6ed1e5017b5a078037149552d8483fb2690dc6d1
Daniel <dsw667@live.com>
2021-12-10 02:05:24 -0500

    connected databse and create schema + linked pages

--- server.js
+++ server.js
@@ -1,12 +1,90 @@
 //dependencies: nodemon, env, express, bcrypt, mongoose, passport, passport-local, 
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
+//express configuration
+app.use(express.static(__dirname + '/views'));
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false}))
 
+//connection to database
+mongoConnect()
+
+//creates schema and collection for db (CONNECTED)
+const user = createSchema();
+const Users = mongoose.model('Users', user)
+
+/*
+    TODO:
+        have to change the a href for all the files
+*/
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
+
+app.get('/calendar.html', (req, res) => {
+    res.render('calendar.html')
+})
+app.get('/notes.html', (req, res) => {
+    res.render('notes.html')
+})
+
 
 //TODO:
-//get views to work with css
-//set updatabase with ability to add things
-//start login system
+//get views to work with css--
+//set up database with ability to add things--
+//implement login system
+//implement registering system
+//figure out how to take from forms in html into the 
+//then take from database to put into the html
+
+
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
+
+
+/****************************************************
+ *          Mongoose Database functions             *
+ ****************************************************/
+
+async function mongoConnect() {
+    const url = process.env.MONGODB_URL || "mongodb+srv://Final:jehziz-zomwa1-wYbxut@cluster0.20nue.mongodb.net/Accounts?retryWrites=true&w=majority";
+
+    //connection to database
+    await mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+}
+
+//creates the schema for the database
+function createSchema() {
+    const user = new mongoose.Schema({
+        userName: String,
+        password: String,
+        email: String,
+        calendar: {
+            date: Number, 
+            content: [String]
+        },
+        deadlines: [{ 
+            isChecked: Boolean, 
+            content: String, 
+            dueDate: Number 
+        }],
+        reminders: [{
+            content: String
+        }],
+        notes: [{
+            title: String,
+            content: [String]
+        }],
+        mood: [{
+            date: Number,
+            mood: Number
+        }]
+    })
+
+    return user
+}
