9b06e1012b0acdeacbd60d5851c1291e60766fce
Daniel <dsw667@live.com>
2021-12-11 23:16:52 -0500

    asd

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false}))
 
 //connection to database
-mongoConnect()
+//mongoConnect()
 
 //creates schema and collection for db (CONNECTED)
 //const user = createSchema();
